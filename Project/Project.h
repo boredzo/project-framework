c39e406b2be8b05e76d100887aab96b2d2e1d9b0
Peter Hosey <prh@Four-Sided-Cheese-Grater.local>
2011-10-18 09:25:24 -0700

    Initial Commit

---
+++ Project/Project.h
@@ -0,0 +1,13 @@
+//
+//  Project.h
+//  Project
+//
+//  Created by Peter Hosey on 2011-10-18.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Project : NSObject
+
+@end
---
+++ Project/Project.m
@@ -0,0 +1,23 @@
+//
+//  Project.m
+//  Project
+//
+//  Created by Peter Hosey on 2011-10-18.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Project.h"
+
+@implementation Project
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
